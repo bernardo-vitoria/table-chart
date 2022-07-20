8f98339033dda713fbac756ab639d427f864563f
Bernardo Vitória <38571894+bernardo-vitoria@users.noreply.github.com>
2022-07-20 12:14:26 +0100

    Features docker (#9)
    * selected metric fix
    
    * app dockerfile
    
    * ignore score and api docker file

--- app/src/pages/mainPage/components/metricsTable/components/metricsRow/MetricsRow.tsx
+++ app/src/pages/mainPage/components/metricsTable/components/metricsRow/MetricsRow.tsx
@@ -8,8 +8,16 @@ type MetricsRowProps = {
 
 const MetricsRow: React.FC<MetricsRowProps> = ({ metric, onClick }) => {
   const { label, description, value, category, type } = metric;
+  const shouldIgnore = metric.id !== "oee";
+  const handleOnClick = shouldIgnore ? onClick : () => {};
+  const metricsRowStyle = shouldIgnore ? "metrics-row" : "";
+
   return (
-    <tr className="metrics-row" data-testid="metricsRow" onClick={onClick}>
+    <tr
+      className={metricsRowStyle}
+      data-testid="metricsRow"
+      onClick={handleOnClick}
+    >
       <td>{label}</td>
       <td>{description}</td>
       <td>{value}</td>
